3e325928ee6764af30cd1d2097a4640a10e5e848
larsfriese <lars.friese@onlinehome.de>
2022-09-26 13:02:06 +0000

    Matrix script can now compute determinants

--- other_stuff/matrix.py
+++ other_stuff/matrix.py
@@ -1,6 +1,6 @@
-matrix = [[2, 3, 4], [6, 7, 8], [4, 6, 2]] # rows
+import copy
 
-type_of_square_matrix = len(matrix)
+matrix = [[0, 3, 4, 3], [0, 6, 8, 6], [-2, -1, 4, 0], [-2, -7, 4, 9]] # rows
 
 def det2(matrix):
     return (matrix[0][0]*matrix[1][1] - matrix[0][0]*matrix[1][1])
@@ -10,30 +10,41 @@ def det3(matrix):
             matrix[0][2]*matrix[1][0]*matrix[2][1] - matrix[0][2]*matrix[1][1]*matrix[2][0] -
             matrix[0][1]*matrix[1][0]*matrix[2][2] - matrix[0][0]*matrix[1][2]*matrix[2][1])
 
-def min(matrix, i, j): # i'th column, j'th row
-    del matrix[j]
-    for x in matrix:
-        del matrix[x][i]
-    return matrix
+def minorant(matrix, i, j): # i'th column, j'th row
+    matrix_new = copy.deepcopy(matrix)
+    for c in range(0, len(matrix_new)):
+        del matrix_new[c][i]
+    del matrix_new[j]
+    return matrix_new
 
 def det(matrix, j):
-    if len(i) != type_of_square_matrix:
-        print('Not square.')
-        exit(0)
-    
+    type_of_square_matrix = len(matrix)
+    for i in matrix:
+        if len(i) != type_of_square_matrix:
+            raise Exception('Matrix not square.')
+
     if len(matrix) == 1:
         return matrix[0]
     elif len(matrix) == 2:
-        return det3(matrix)
+        return det2(matrix)
     elif len(matrix) == 3:
         return det3(matrix)
     else:
+        # Laplas'sche entwicklung nach j'ter Zeile
         sum = 0
-        for k in range(type_of_square_matrix, 1):
-            sum += (-1)**(k+j) * matrix[j][k] * det(min(matrix, k, j))
+        for k in range(0, type_of_square_matrix):
+            print(matrix)
+            sum += (-1)**(k+j) * matrix[j][k] * det(minorant(matrix, k, j), 1)
+        return sum
 
-# def check_selfadj():
-    
+def transpose(matrix):
+    return list(map(list, zip(*matrix)))
+
+def self_adjugated(matrix):
+    return True if matrix == transpose(matrix) else False
+
+def eigenvalues(matrix):
+    return
 
 if __name__ == '__main__':
     print(det(matrix, 1))
